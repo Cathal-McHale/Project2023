e8b26c798d72ffbce21e8653adf2a81210a9e45d
unknown <g00402138@atu.ie>
2023-05-12 10:40:45 +0000

    Project Complete

--- src/app/go-to-settings-page/go-to-settings-page.page.spec.ts
+++ src/app/go-to-settings-page/go-to-settings-page.page.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { GoToSettingsPagePage } from './go-to-settings-page.page';
 
 describe('GoToSettingsPagePage', () => {
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-
+import { Browser } from '@capacitor/browser';
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+selector: 'app-home',
+templateUrl: 'home.page.html',
+styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-stories:any[]=[];
-  constructor() {}
-  
+constructor() { }
+async openBrowser() {
+await Browser.open({ url: 'http://capacitorjs.com/' });
+};
 }
+
+
